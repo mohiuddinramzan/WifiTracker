a2aa3e6d3b66b67ab58ab170ff2ac37bc84ab72a
mohiuddinramzan2 <mohiuddinramzan2@gmail.com>
2026-08-20 07:29:29 +0600

    fix build errors

--- app/src/main/java/com/wifitracker/app/ui/components/BarChart.kt
+++ app/src/main/java/com/wifitracker/app/ui/components/BarChart.kt
@@ -12,8 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.RoundRect
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
